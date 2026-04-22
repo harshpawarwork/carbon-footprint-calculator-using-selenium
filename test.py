792f36ea491e0aecfcbd609f92ac2e15edce550b
harsh <hpawar9881@gmail.com>
2026-04-22 21:10:17 +0530

    Updates selenium file

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ driver = webdriver.Chrome()
 
 driver.get("file:///C:/Users/Harsh/OneDrive/Desktop/carbon footprint calculator using selenium/carbon.html")
 
-time.sleep(2)
+time.sleep(1)
 
 driver.find_element(By.ID, "fuel").send_keys("50")
 driver.find_element(By.ID, "electricity").send_keys("40")
@@ -15,5 +15,7 @@ driver.find_element(By.ID, "waste").send_keys("10")
 driver.find_element(By.TAG_NAME, "button").click()
 
 time.sleep(5)
+
+print("Test completed successfully")
 
 driver.quit()
